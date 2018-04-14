c8da9ed9e8cfac14f39d86b274b216d93aa152c1
Joostlek <joostlek@outlook.com>
2018-04-14 23:54:31 +0200

    Added Project Component

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatCardModule, MatDividerModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { ProjectComponent } from './project/project.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatCardModule,
+    MatDividerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
